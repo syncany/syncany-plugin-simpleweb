f9309116d65e7201a5361b5cc31a0b024b39a8ce
Philipp Heckel <philipp.heckel@gmail.com>
2014-06-24 17:57:38 +0200

    Squashed 'core/' changes from f3680ec..a66d5df
    a66d5df Changes to work with plugins
    c86dd17 Prevent binary file processing of resources
    404099d Removed web interface to own module
    bf33c8d Remove old test JS/HTML code, introduce web server config
    89fa9a7 Merge branch 'develop' into feature/daemon
    09234a7 Merge branch 'feature/anyplugin' into feature/daemon
    0c06126 Fix list/set
    c247794 Merge branch 'feature/anyplugin' into feature/daemon
    210833b Adjust web interface plugin to new plugin layout
    b9eaf91 Merge branch 'feature/anyplugin' into feature/daemon
    00b2faf Merge branch 'develop' into feature/daemon
    6803026 Add working file download of any version
    be3bcdd Merge branch 'develop' into feature/daemon
    1b38ff3 Merge branch 'develop' into feature/daemon
    3f52023 Restore request/response; adjust web interface
    6e93231 Let the CLI talk to the daemon via WS if it's handled by the daemon
    a85df00 Merge branch 'develop' into feature/daemon
    f78f688 Merge branch 'develop' into feature/daemon
    63c5af3 Merge branch 'develop' into feature/daemon
    be299eb Merge branch 'develop' into feature/daemon
    0ca35b3 Minor changes
    a4081d2 Merge branch 'develop' into feature/daemon
    7c2574d Allow date browsing
    7689860 Merge branch 'develop' into feature/daemon
    7a28704 Merge branch 'feature/newlog' into feature/daemon
    bedb5d4 Fix newlog
    8aa3bc9 Merge branch 'feature/newlog' into feature/daemon
    cd43180 Merge branch 'develop' into feature/daemon
    9656f19 Remove binary response stuff
    a6160cd Merge develop
    b044feb Make web interface a plugin
    72dacc6 Add new events support to web ui
    da1e5bb Merge branch 'develop' into feature/daemon
    528c25a Cont'd web interface
    8cc0e64 Grouping of table by file type, finally
    0941704 Cont'd cleaning up JS code
    6562e4d Scrolling and stuff
    8027cdc Added tree and table with subentries, added slider for date selection
    77d90ac Added context menu to table view, removed files from tree view
    df56385 Embedded web interface; switched to Undertow web server / web socket server
    75c28aa Cont'd work on web interface
    3c1945e Nicer menu, status bar
    9fe646c Add datatable
    0753a57 Simple web interface
    a9712b7 Reply only to one client
    a1eb5fd Continued JavaScript frontend; working file tree with multiple roots
    469ec67 Merge branch 'develop' into feature/daemon
    a0538a0 Merge branch 'develop' into feature/daemon
    6a38fe1 Merge develop
    991fb06 Merge branch 'develop' into feature/daemon
    c45156e Merge branch 'develop' into feature/daemon
    8541fac Merge branch 'feature/daemon' of github.com:syncany/syncany into feature/daemon
    83de56a Merge branch 'develop' into feature/daemon
    4ddc37e Merge branch 'develop' into feature/daemon
    d0da3ba Merge branch 'develop' into feature/daemon
    817e6ac Playing around with websocket/js-based download
    9d3598f Add websocket file transfer test to web/js client
    a7f445d Playing around with binary data in websocket return
    d8b9d49 Add XML root names as type distinguisher
    3c8540f Moving things around
    2517c95 Working fileTree WS request/response
    8984609 Continue WebSocket development; Re-introduce Guava EventBus
    39ca7c5 Merge branch 'develop' into feature/daemon
    4f102ff Merge branch 'develop' into feature/daemon
    0c774fc Merge branch 'feature/daemon' of github.com:syncany/syncany into feature/daemon
    a5944b2 Merge branch 'develop' into feature/daemon
    d295d1e Re-enable websocket
    REVERT: f3680ec Update changelog
    REVERT: 4ae39fc Fix failing tests
    REVERT: 67ddd20 Fixing a few (not all) tests
    
    git-subtree-dir: core
    git-subtree-split: a66d5df170863661a7a54069c6e7c5ee581a3bca

---
+++ syncany-daemon/src/main/java/org/syncany/operations/daemon/DaemonWebServer.java
@@ -0,0 +1,330 @@
+/*
+ * Syncany, www.syncany.org
+ * Copyright (C) 2011-2013 Philipp C. Heckel <philipp.heckel@gmail.com> 
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.syncany.operations.daemon;
+
+import static io.undertow.Handlers.path;
+import static io.undertow.Handlers.websocket;
+import io.undertow.Undertow;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.websockets.WebSocketConnectionCallback;
+import io.undertow.websockets.core.AbstractReceiveListener;
+import io.undertow.websockets.core.BufferedTextMessage;
+import io.undertow.websockets.core.StreamSourceFrameChannel;
+import io.undertow.websockets.core.WebSocketChannel;
+import io.undertow.websockets.core.WebSockets;
+import io.undertow.websockets.spi.WebSocketHttpExchange;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+import org.apache.commons.io.IOUtils;
+import org.syncany.config.to.DaemonConfigTO;
+import org.syncany.operations.daemon.messages.BadRequestResponse;
+import org.syncany.operations.daemon.messages.GetFileResponse;
+import org.syncany.operations.daemon.messages.GetFileResponseInternal;
+import org.syncany.operations.daemon.messages.MessageFactory;
+import org.syncany.operations.daemon.messages.Request;
+import org.syncany.operations.daemon.messages.Response;
+import org.syncany.plugins.Plugins;
+import org.syncany.plugins.web.WebInterfacePlugin;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.eventbus.Subscribe;
+
+public class DaemonWebServer {
+	private static final Logger logger = Logger.getLogger(DaemonWebServer.class.getSimpleName());
+	private static final Pattern WEBSOCKET_ALLOWED_ORIGIN_HEADER = Pattern.compile("^https?://(localhost|127\\.\\d+\\.\\d+\\.\\d+):\\d+$");
+
+	private Undertow webServer;
+	private DaemonEventBus eventBus;
+
+	private Cache<Integer, WebSocketChannel> requestIdWebSocketCache;
+	private Cache<Integer, HttpServerExchange> requestIdRestSocketCache;
+	private Cache<String, File> fileTokenTempFileCache;
+	
+	private List<WebSocketChannel> clientChannels;
+
+	public DaemonWebServer(DaemonConfigTO daemonConfig) {
+		this.clientChannels = new ArrayList<WebSocketChannel>();
+
+		initCaches();
+		initEventBus();
+		initServer(daemonConfig.getWebServer().getHost(), daemonConfig.getWebServer().getPort());
+	}
+
+	public void start() throws ServiceAlreadyStartedException {
+		webServer.start();
+	}
+
+	public void stop() {
+		try {
+			webServer.stop();
+		}
+		catch (Exception e) {
+			logger.log(Level.SEVERE, "Could not stop websocket server.", e);
+		}
+	}
+
+	private void initCaches() {
+		requestIdWebSocketCache = CacheBuilder.newBuilder().maximumSize(10000)
+				.concurrencyLevel(2).expireAfterAccess(1, TimeUnit.MINUTES).build();
+		
+		requestIdRestSocketCache = CacheBuilder.newBuilder().maximumSize(10000)
+				.concurrencyLevel(2).expireAfterAccess(1, TimeUnit.MINUTES).build();
+		
+		fileTokenTempFileCache = CacheBuilder.newBuilder().maximumSize(10000)
+				.concurrencyLevel(2).expireAfterAccess(1, TimeUnit.MINUTES).build();
+	}
+
+	private void initEventBus() {
+		eventBus = DaemonEventBus.getInstance();
+		eventBus.register(this);
+	}
+
+	private void initServer(String host, int port) {
+		webServer = Undertow
+			.builder()
+			.addHttpListener(port, host)
+			.setHandler(path()
+				.addPrefixPath("/api/ws", websocket(new InternalWebSocketHandler()))
+				.addPrefixPath("/api/rs", new InternalRestHandler())
+				.addPrefixPath("/", new InternalWebInterfaceHandler())
+			).build();
+	}
+
+	private void handleWebSocketRequest(WebSocketChannel clientSocket, String message) {
+		logger.log(Level.INFO, "Web socket message received: " + message);
+
+		try {
+			Request request = MessageFactory.createRequest(message);
+
+			synchronized (requestIdWebSocketCache) {
+				requestIdWebSocketCache.put(request.getId(), clientSocket);	
+			}
+			
+			eventBus.post(request);
+		}
+		catch (Exception e) {
+			logger.log(Level.WARNING, "Invalid request received; cannot serialize to Request.", e);
+			eventBus.post(new BadRequestResponse(-1, "Invalid request."));
+		}
+	}
+	
+	private void handleRestRequest(HttpServerExchange exchange) throws IOException {
+		logger.log(Level.INFO, "HTTP request received:" + exchange.getRelativePath());
+
+		exchange.startBlocking();			
+
+		if (exchange.getRelativePath().startsWith("/file/")) {	
+			String tempFileToken = exchange.getRelativePath().substring("/file/".length());
+			File tempFile = fileTokenTempFileCache.asMap().get(tempFileToken);
+			
+			logger.log(Level.INFO, "- Temp file: " + tempFileToken);
+			
+			IOUtils.copy(new FileInputStream(tempFile), exchange.getOutputStream());
+			
+			exchange.endExchange();
+		}
+		else {	
+			String message = IOUtils.toString(exchange.getInputStream());
+			logger.log(Level.INFO, "REST message received: " + message);
+	
+			try {
+				Request request = MessageFactory.createRequest(message);
+	
+				synchronized (requestIdRestSocketCache) {
+					requestIdRestSocketCache.put(request.getId(), exchange);	
+				}
+				
+				eventBus.post(request);
+			}
+			catch (Exception e) {
+				logger.log(Level.WARNING, "Invalid request received; cannot serialize to Request.", e);
+				eventBus.post(new BadRequestResponse(-1, "Invalid request."));
+			}
+		}
+	}
+
+	private void sendBroadcast(String message) {
+		synchronized (clientChannels) {
+			for (WebSocketChannel clientChannel : clientChannels) {
+				sendTo(clientChannel, message);
+			}
+		}
+	}
+
+	private void sendTo(WebSocketChannel clientChannel, String message) {
+		logger.log(Level.INFO, "Sending message to " + clientChannel + ": " + message);
+		WebSockets.sendText(message, clientChannel, null);
+	}
+	
+	private void sendTo(HttpServerExchange serverExchange, String message) {
+		logger.log(Level.INFO, "Sending message to " + serverExchange.getHostAndPort() + ": " + message);
+		
+		serverExchange.getResponseSender().send(message);
+		serverExchange.endExchange();
+	}
+
+	@Subscribe
+	public void onGetFileResponseInternal(GetFileResponseInternal fileResponseInternal) {
+		File tempFile = fileResponseInternal.getTempFile();
+		GetFileResponse fileResponse = fileResponseInternal.getFileResponse();
+		
+		fileTokenTempFileCache.asMap().put(fileResponse.getTempToken(), tempFile);
+		eventBus.post(fileResponse);
+	}
+
+	@Subscribe
+	public void onResponse(Response response) {
+		try {
+			// Serialize response
+			String responseMessage = MessageFactory.toResponse(response);
+
+			// Send to one or many receivers
+			boolean responseWithoutRequest = response.getRequestId() == null || response.getRequestId() <= 0;
+
+			if (responseWithoutRequest) {
+				sendBroadcast(responseMessage);
+			}
+			else {
+				HttpServerExchange responseServerExchange = requestIdRestSocketCache.asMap().get(response.getRequestId());
+				WebSocketChannel responseToClientSocket = requestIdWebSocketCache.asMap().get(response.getRequestId());
+
+				if (responseServerExchange != null) {
+					sendTo(responseServerExchange, responseMessage);
+				}				
+				else if (responseToClientSocket != null) {
+					sendTo(responseToClientSocket, responseMessage);
+				}
+				else {
+					logger.log(Level.WARNING, "Cannot send message, because request ID in response is unknown or timed out." + responseMessage);
+				}
+			}
+		}
+		catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	private class InternalWebSocketHandler implements WebSocketConnectionCallback {
+		@Override
+		public void onConnect(WebSocketHttpExchange exchange, WebSocketChannel channel) {
+			// Validate origin header (security!)
+			String originHeader = exchange.getRequestHeader("Origin");
+			boolean allowedOriginHeader = originHeader == null || WEBSOCKET_ALLOWED_ORIGIN_HEADER.matcher(originHeader).matches();
+			
+			if (!allowedOriginHeader) {
+				logger.log(Level.INFO, channel.toString() + " disconnected due to invalid origin header: " + originHeader);
+				exchange.close();
+			}
+			else {
+				channel.getReceiveSetter().set(new AbstractReceiveListener() {
+					@Override
+					protected void onFullTextMessage(WebSocketChannel clientChannel, BufferedTextMessage message) {
+						handleWebSocketRequest(clientChannel, message.getData());
+					}
+	
+					@Override
+					protected void onError(WebSocketChannel webSocketChannel, Throwable error) {
+						logger.log(Level.INFO, "Server error : " + error.toString());
+					}
+	
+					@Override
+					protected void onClose(WebSocketChannel clientChannel, StreamSourceFrameChannel streamSourceChannel) throws IOException {
+						logger.log(Level.INFO, clientChannel.toString() + " disconnected");
+	
+						synchronized (clientChannels) {
+							clientChannels.remove(clientChannel);
+						}
+					}
+				});
+	
+				synchronized (clientChannels) {
+					clientChannels.add(channel);
+				}
+	
+				channel.resumeReceives();
+			}
+		}
+	}
+
+	private class InternalRestHandler implements HttpHandler {
+		@Override
+		public void handleRequest(final HttpServerExchange exchange) throws Exception {
+			handleRestRequest(exchange);
+		}
+	}
+	
+	public class InternalWebInterfaceHandler implements HttpHandler {
+		private List<WebInterfacePlugin> webInterfacePlugins;
+		private WebInterfacePlugin webInterfacePlugin;
+		private HttpHandler requestHandler;
+		
+		public InternalWebInterfaceHandler() {
+			webInterfacePlugins = Plugins.list(WebInterfacePlugin.class);
+			
+			if (webInterfacePlugins.size() == 1) {
+				initWebInterfacePlugin();
+			}
+		}
+
+		private void initWebInterfacePlugin() {
+			try {				
+				webInterfacePlugin = webInterfacePlugins.iterator().next();
+				requestHandler = webInterfacePlugin.createRequestHandler();
+				
+				webInterfacePlugin.start();
+			}
+			catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		@Override
+		public void handleRequest(HttpServerExchange exchange) throws Exception {
+			if (requestHandler != null) {
+				handleRequestWithResourceHandler(exchange);
+			}
+			else {
+				handleRequestNoHandler(exchange);
+			}
+		}
+
+		private void handleRequestWithResourceHandler(HttpServerExchange exchange) throws Exception {
+			requestHandler.handleRequest(exchange);
+		}
+
+		private void handleRequestNoHandler(HttpServerExchange exchange) {
+			if (webInterfacePlugins.size() == 0) {
+				exchange.getResponseSender().send("No web interface configured.");
+			}
+			else {
+				exchange.getResponseSender().send("Only one web interface can be loaded.");
+			}
+		}
+	}
+}
--- syncany-daemon/src/main/java/org/syncany/operations/daemon/WatchOperationThread.java
+++ syncany-daemon/src/main/java/org/syncany/operations/daemon/WatchOperationThread.java
@@ -17,16 +17,60 @@
  */
 package org.syncany.operations.daemon;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.PrintStream;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.syncany.Client;
+import org.syncany.cli.Command;
+import org.syncany.cli.CommandFactory;
 import org.syncany.config.Config;
 import org.syncany.config.ConfigException;
-import org.syncany.config.ConfigHelper;
+import org.syncany.database.ChunkEntry.ChunkChecksum;
+import org.syncany.database.DatabaseVersionHeader;
+import org.syncany.database.FileContent;
+import org.syncany.database.FileVersion;
+import org.syncany.database.FileVersion.FileType;
+import org.syncany.database.MultiChunkEntry.MultiChunkId;
+import org.syncany.database.ObjectId;
+import org.syncany.database.PartialFileHistory.FileHistoryId;
+import org.syncany.database.SqlDatabase;
+import org.syncany.operations.Assembler;
+import org.syncany.operations.Downloader;
+import org.syncany.operations.daemon.messages.BadRequestResponse;
+import org.syncany.operations.daemon.messages.CliRequest;
+import org.syncany.operations.daemon.messages.CliResponse;
+import org.syncany.operations.daemon.messages.GetDatabaseVersionHeadersRequest;
+import org.syncany.operations.daemon.messages.GetDatabaseVersionHeadersResponse;
+import org.syncany.operations.daemon.messages.GetFileHistoryRequest;
+import org.syncany.operations.daemon.messages.GetFileHistoryResponse;
+import org.syncany.operations.daemon.messages.GetFileRequest;
+import org.syncany.operations.daemon.messages.GetFileResponse;
+import org.syncany.operations.daemon.messages.GetFileResponseInternal;
+import org.syncany.operations.daemon.messages.GetFileTreeRequest;
+import org.syncany.operations.daemon.messages.GetFileTreeResponse;
+import org.syncany.operations.daemon.messages.RestoreRequest;
+import org.syncany.operations.daemon.messages.RestoreResponse;
+import org.syncany.operations.daemon.messages.WatchEventResponse;
+import org.syncany.operations.daemon.messages.WatchRequest;
+import org.syncany.operations.restore.RestoreOperation;
+import org.syncany.operations.restore.RestoreOperationOptions;
+import org.syncany.operations.restore.RestoreOperationResult;
 import org.syncany.operations.watch.WatchOperation;
 import org.syncany.operations.watch.WatchOperationListener;
 import org.syncany.operations.watch.WatchOperationOptions;
+import org.syncany.plugins.transfer.TransferManager;
+import org.syncany.util.StringUtil;
+
+import com.google.common.eventbus.Subscribe;
 
 /**
  * The watch operation thread runs a {@link WatchOperation} in a thread. The 
@@ -35,30 +79,37 @@ import org.syncany.operations.watch.WatchOperationOptions;
  * 
  * @author Philipp C. Heckel <philipp.heckel@gmail.com>
  */
-public class WatchOperationThread {
+public class WatchOperationThread implements WatchOperationListener {
 	private static final Logger logger = Logger.getLogger(WatchOperationThread.class.getSimpleName());
-
+	
 	private Config config;
+	private File portFile;
 	private Thread watchThread;
 	private WatchOperation watchOperation;
+	private DaemonEventBus eventBus;
+	
+	private SqlDatabase localDatabase;
 
-	public WatchOperationThread(File localDir, WatchOperationOptions watchOperationOptions, WatchOperationListener listener) throws ConfigException {
-		File configFile = ConfigHelper.findLocalDirInPath(localDir);
+	public WatchOperationThread(Config config, WatchOperationOptions watchOperationOptions) throws ConfigException {
+		this.config = config;
+		this.portFile = new File(config.getAppDir(), "port");
+		this.watchOperation = new WatchOperation(config, watchOperationOptions, this);
 		
-		if (configFile == null) {
-			throw new ConfigException("Config file in folder " + localDir + " not found.");
-		}
+		this.localDatabase = new SqlDatabase(config);
 		
-		this.config = ConfigHelper.loadConfig(configFile);
-		this.watchOperation = new WatchOperation(config, watchOperationOptions, listener);
+		this.eventBus = DaemonEventBus.getInstance();
+		this.eventBus.register(this);
 	}
 	
-	public void start() throws ServiceAlreadyStartedException {
+	public void start() {
 		watchThread = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				try {
 					logger.log(Level.INFO, "STARTING watch at" + config.getLocalDir());
+					
+					portFile.createNewFile();
+					portFile.deleteOnExit();
 					
 					watchOperation.execute();
 					
@@ -75,6 +126,212 @@ public class WatchOperationThread {
 
 	public void stop() {
 		watchOperation.stop();
+		portFile.delete();
+
 		watchThread = null;
+	}
+	
+	@Subscribe
+	public void onRequestReceived(WatchRequest watchRequest) {		
+		File requestRootFolder = new File(watchRequest.getRoot());
+		boolean localDirMatches = requestRootFolder.equals(config.getLocalDir());
+		
+		if (localDirMatches) {
+			logger.log(Level.INFO, "Received " + watchRequest);
+			
+			if (watchRequest instanceof GetFileTreeRequest) {
+				handleGetFileTreeRequest((GetFileTreeRequest) watchRequest);			
+			}
+			else if (watchRequest instanceof GetFileHistoryRequest) {
+				handleGetFileHistoryRequest((GetFileHistoryRequest) watchRequest);			
+			}
+			else if (watchRequest instanceof GetFileRequest) {
+				handleGetFileRequest((GetFileRequest) watchRequest);
+			}
+			else if (watchRequest instanceof GetDatabaseVersionHeadersRequest) {
+				handleGetDatabaseVersionHeadersRequest((GetDatabaseVersionHeadersRequest) watchRequest);			
+			}
+			else if (watchRequest instanceof RestoreRequest) {
+				handleRestoreRequest((RestoreRequest) watchRequest);			
+			}
+			else if (watchRequest instanceof CliRequest) {
+				handleCliRequest((CliRequest) watchRequest);
+			}
+			else {
+				eventBus.post(new BadRequestResponse(watchRequest.getId(), "Invalid watch request for root."));
+			}
+		}		
+	}
+
+	private void handleGetFileRequest(GetFileRequest fileRequest) {
+		try {
+			FileHistoryId fileHistoryId = FileHistoryId.parseFileId(fileRequest.getFileHistoryId());
+			long version = fileRequest.getVersion();
+			
+			FileVersion fileVersion = localDatabase.getFileVersion(fileHistoryId, version);			
+			FileContent fileContent = localDatabase.getFileContent(fileVersion.getChecksum(), true);
+			Map<ChunkChecksum, MultiChunkId> multiChunks = localDatabase.getMultiChunkIdsByChecksums(fileContent.getChunks());
+						
+			TransferManager transferManager = config.getTransferPlugin().createTransferManager(config.getConnection());			
+			Downloader downloader = new Downloader(config, transferManager);
+			Assembler assembler = new Assembler(config, localDatabase);
+			
+			downloader.downloadAndDecryptMultiChunks(new HashSet<MultiChunkId>(multiChunks.values()));
+			
+			File tempFile = assembler.assembleToCache(fileVersion);			
+			String tempFileToken = StringUtil.toHex(ObjectId.secureRandomBytes(40));
+			
+			GetFileResponse fileResponse = new GetFileResponse(fileRequest.getId(), fileRequest.getRoot(), tempFileToken);
+			GetFileResponseInternal fileResponseInternal = new GetFileResponseInternal(fileResponse, tempFile);
+
+			eventBus.post(fileResponseInternal);
+		}
+		catch (Exception e) {
+			logger.log(Level.WARNING, "Cannot reassemble file.", e);
+			eventBus.post(new BadRequestResponse(fileRequest.getId(), "Cannot reassemble file."));
+		}		
+	}
+
+	private void handleRestoreRequest(RestoreRequest restoreRequest) {
+		RestoreOperationOptions restoreOptions = new RestoreOperationOptions();
+		
+		restoreOptions.setFileHistoryId(FileHistoryId.parseFileId(restoreRequest.getFileHistoryId()));
+		restoreOptions.setFileVersion(restoreRequest.getVersion());
+		
+		try {
+			RestoreOperationResult restoreResult = new RestoreOperation(config, restoreOptions).execute();
+			
+			RestoreResponse restoreResponse = new RestoreResponse(restoreRequest.getId(), restoreResult.getTargetFile());
+			eventBus.post(restoreResponse);								
+		}
+		catch (Exception e) {
+			eventBus.post(new BadRequestResponse(restoreRequest.getId(), "Cannot restore file."));
+		}
+	}
+
+	private void handleCliRequest(CliRequest cliRequest) {		
+		try {
+			Command command = CommandFactory.getInstance(cliRequest.getCommand());			
+			String[] commandArgs = cliRequest.getCommandArgs().toArray(new String[0]); 
+			
+			ByteArrayOutputStream cliOutputStream = new ByteArrayOutputStream();
+			
+			Client client = new Client();
+			client.setConfig(config);
+			
+			command.setOut(new PrintStream(cliOutputStream));
+			command.setClient(client);
+			command.setLocalDir(config.getLocalDir());
+			
+			command.execute(commandArgs);
+			
+			String cliOutput = cliOutputStream.toString();
+			
+			CliResponse cliResponse = new CliResponse(cliRequest.getId(), cliOutput);
+			eventBus.post(cliResponse);
+			
+			cliOutputStream.close();
+		}
+		catch (Exception e) {
+			eventBus.post(new BadRequestResponse(cliRequest.getId(), e.getMessage()));
+		}
+		
+	}
+
+	private void handleGetFileTreeRequest(GetFileTreeRequest fileTreeRequest) {
+		try {
+			String prefixLikeQuery = fileTreeRequest.getPrefix() + "%";
+			Date date = (fileTreeRequest.getDate() != null) ? new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSZ").parse(fileTreeRequest.getDate()) : null;
+			
+			Map<String, FileVersion> fileTree = localDatabase.getFileTree(prefixLikeQuery, date, false, (FileType[]) null);
+			GetFileTreeResponse fileTreeResponse = new GetFileTreeResponse(fileTreeRequest.getId(), fileTreeRequest.getRoot(), fileTreeRequest.getPrefix(), new ArrayList<FileVersion>(fileTree.values()));
+			
+			eventBus.post(fileTreeResponse);	
+		}
+		catch (Exception e) {
+			eventBus.post(new BadRequestResponse(fileTreeRequest.getId(), "Invalid request: " + e.getMessage()));
+		}	
+	}
+	
+	private void handleGetFileHistoryRequest(GetFileHistoryRequest fileHistoryRequest) {
+		FileHistoryId fileHistoryId = FileHistoryId.parseFileId(fileHistoryRequest.getFileHistoryId());
+		List<FileVersion> fileHistory = localDatabase.getFileHistory(fileHistoryId);
+		GetFileHistoryResponse fileHistoryRespose = new GetFileHistoryResponse(fileHistoryRequest.getId(), fileHistoryRequest.getRoot(), fileHistory);
+		
+		eventBus.post(fileHistoryRespose);
+	}
+	
+	private void handleGetDatabaseVersionHeadersRequest(GetDatabaseVersionHeadersRequest headersRequest) {
+		List<DatabaseVersionHeader> databaseVersionHeaders = localDatabase.getNonEmptyDatabaseVersionHeaders(); 
+		GetDatabaseVersionHeadersResponse headersResponse = new GetDatabaseVersionHeadersResponse(headersRequest.getId(), headersRequest.getRoot(), databaseVersionHeaders);
+		
+		eventBus.post(headersResponse);
+	}
+
+	@Override
+	public void onUploadStart(int fileCount) {
+		String root = config.getLocalDir().getAbsolutePath();
+		String action = "UPLOAD_START";
+		
+		eventBus.post(new WatchEventResponse(root, action));
+	}
+
+	@Override
+	public void onUploadFile(String fileName, int fileNumber) {
+		String root = config.getLocalDir().getAbsolutePath();
+		String action = "UPLOAD_FILE";
+		String subject = fileName;
+		
+		eventBus.post(new WatchEventResponse(root, action, subject));
+	}
+
+	@Override
+	public void onUploadEnd() {
+		String root = config.getLocalDir().getAbsolutePath();
+		String action = "UPLOAD_END";
+		
+		eventBus.post(new WatchEventResponse(root, action));
+	}
+
+	@Override
+	public void onIndexStart(int fileCount) {
+		String root = config.getLocalDir().getAbsolutePath();
+		String action = "INDEX_START";
+		
+		eventBus.post(new WatchEventResponse(root, action));
+	}
+
+	@Override
+	public void onIndexFile(String fileName, int fileNumber) {
+		String root = config.getLocalDir().getAbsolutePath();
+		String action = "INDEX_FILE";
+		String subject = fileName;
+		
+		eventBus.post(new WatchEventResponse(root, action, subject));
+	}
+	
+	@Override
+	public void onIndexEnd() {
+		String root = config.getLocalDir().getAbsolutePath();
+		String action = "INDEX_END";
+		
+		eventBus.post(new WatchEventResponse(root, action));
+	}
+
+	@Override
+	public void onDownloadStart(int fileCount) {
+		String root = config.getLocalDir().getAbsolutePath();
+		String action = "DOWNLOAD_START";
+		
+		eventBus.post(new WatchEventResponse(root, action));
+	}
+
+	@Override
+	public void onDownloadFile(String fileName, int fileNumber) {
+		String root = config.getLocalDir().getAbsolutePath();
+		String action = "DOWNLOAD_FILE";
+		String subject = fileName;
+		
+		eventBus.post(new WatchEventResponse(root, action, subject));
 	}
 }
